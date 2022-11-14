aea01eadb355a72752f9bf7a773241e8fb293a54
maulanazulkifar <maulanazulkifar@gmail.com>
2022-11-15 06:16:14 +0700

    first commit

---
+++ src/components/homepage/homepageheader/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const HomePageHeader = (props) => {
+  return (
+    <div>
+      <div>
+        <img className={'logo'} src={'logo.png'} alt={'logo jetlympic'}/>
+      </div>
+    </div>
+  )
+}
+
+export default HomePageHeader;
---
+++ src/views/quiz/index.js
@@ -0,0 +1,7 @@
+const Quiz = (props) => {
+  return(
+    <div>INI QUIZ</div>
+  )
+}
+
+export default Quiz;
