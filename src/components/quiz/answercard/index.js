a424463a132900537e7fd83798d651a918ccce6e
maulanazulkifar <maulanazulkifar@gmail.com>
2022-11-15 21:08:49 +0700

    update: add loading screen

--- src/components/quiz/answercard/index.js
+++ src/components/quiz/answercard/index.js
@@ -3,6 +3,7 @@ import "./index.css"
 const AnswerCard = (props) => {
 
   const onClickAnswer = () => {
+    props.onClick()
   }
   return(
       <button onClick={() => onClickAnswer()} className={'card'}>{props.answer}</button>
--- src/views/quiz/index.js
+++ src/views/quiz/index.js
@@ -3,24 +3,35 @@ import "./index.css"
 import Answercard from "../../components/quiz/answercard";
 import {useLocation, useNavigate} from "react-router-dom";
 import get from "../../services/api";
+import Loading from "../../components/loading";
 
 const Quiz = (props) => {
     const { state } = useLocation();
+    const [isLoading, setIsLoading] = useState(false)
     const [question, setQuestion] = useState([{question:{text: ''},options:[]}])
     const [index, setIndex] = useState(1)
     const [totalQuestion, setTotalQuestion]= useState(0)
     const [score, setScore] = useState(0)
 
-    const getData = async () => {
-        get(`jetlimpic/quiz/${state}`).then(res => {
-            setQuestion(res.data)
-            setTotalQuestion(res.data.length)
-        })
+    const getQuestionText = () => {
+      if (state === 'en') {
+          return 'Question'
+      } else if (state === 'id') {
+          return 'Pertanyaan'
+      } else {
+          return '问题'
+      }
     }
 
     useEffect( () => {
+        setIsLoading(true)
         //passing getData method to the lifecycle method
-        getData()
+        get(`jetlimpic/quiz/${state}`).then(res => {
+            setQuestion(res.data)
+            setTotalQuestion(res.data.length)
+            setIsLoading(false);
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const navigate = useNavigate();
@@ -32,17 +43,24 @@ const Quiz = (props) => {
         }
     }
     const onClickAnswer = (isAnswer) => {
-        checkAnswer(isAnswer).then((tempScore) => {
-            if (index<totalQuestion) {
-                setIndex(index + 1);
-            } else {
-                navigate('/result', {state: {score: tempScore?tempScore:score}})
-            }
-        })
+        setIsLoading(true)
+        setTimeout(function(){
+            checkAnswer(isAnswer).then((tempScore) => {
+                if (index<totalQuestion) {
+                    setIndex(index + 1);
+                } else {
+                    navigate('/result', {state: {score: tempScore?tempScore:score}})
+                }
+            })
+            setIsLoading(false)
+        }, 500);
     }
   return(
     <div className={'container'} style={{justifyContent: 'space-between'}}>
-      <div style={{marginTop: 100}}>Question {index}/{totalQuestion}</div>
+        {
+            isLoading ? <Loading></Loading>: ''
+        }
+      <div style={{marginTop: 100}}>{getQuestionText()} {index}/{totalQuestion}</div>
       <div style={{flexGrow: 2}}>
           <div className={'question-text'}>{question.length>0 ? question[index-1].question.text : ''}</div>
           {
@@ -52,7 +70,9 @@ const Quiz = (props) => {
               {
                   question[index - 1].options.map(o => {
                       return (
-                          <Answercard answer={o.text} onClick={() => onClickAnswer(o.is_answer)}/>
+                        <div key={o.id}>
+                            <Answercard answer={o.text} onClick={() => onClickAnswer(o.is_answer)}/>
+                        </div>
                       )
                   })
               }
