eea003504edacffb17ee14ad3fb4c22396ebe621
HANDRA-PC\Handra <handraromel@gmail.com>
2022-11-17 08:52:18 +0700

    update: add quiz page & integrate API

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import {HashRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./views/homepage";
 import Quiz from "./views/quiz";
 import Result from "./views/result";
+import {useEffect, useState} from "react";
+import Alert_desktop from "./components/alert_desktop";
 
 function App() {
+    const isMobile = window.matchMedia("only screen and (max-width: 770px)").matches;
+    console.log(isMobile)
   return (
     <div className="App">
         {/*<HomePage></HomePage>*/}
-        <HashRouter>
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="quiz" element={<Quiz />} />
-            <Route path="result" element={<Result />} />
-          </Routes>
-        </HashRouter>
+        {
+            isMobile?
+                <HashRouter>
+                    <Routes>
+                        <Route index element={<HomePage />} />
+                        <Route path="quiz" element={<Quiz />} />
+                        <Route path="result" element={<Result />} />
+                    </Routes>
+                </HashRouter>:<Alert_desktop/>
+        }
     </div>
   );
 }
